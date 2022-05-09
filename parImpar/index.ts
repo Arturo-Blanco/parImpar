fa72371bc7070b804cf214a67a7f04b98f382d0f
Arturo-Blanco <103155759+Arturo-Blanco@users.noreply.github.com>
2022-05-09 18:47:16 -0300

    Reemplazo "if" por "Switch"

--- parImpar/index.ts
+++ parImpar/index.ts
@@ -1,24 +1,16 @@
 let dato = document.getElementById("dato");
 let btnNum = document.getElementById("btnNum");
 
-btnNum.addEventListener("click" , () => {
-let numIngresado : number = Number(dato.value) ;
+btnNum.addEventListener("click", () => {
+  let numIngresado: number = Number(dato.value);
 
-if(numIngresado % 2 == 0 && numIngresado != 0) {
-  console.log("El numero ingresado es par");
-} else {
-  if(numIngresado == 0) {
-    console.log("El numero ingresado es cero");
+  switch (numIngresado) {
+    case 0:
+      console.log("El numero ingresado es cero");
+      break;
+
+    case numIngresado:
+      console.log("El numero ingresado es par", numIngresado % 2 === 0);
+      break;
   }
-  else {
-  if(numIngresado % 2 != 0 ) {
-    console.log("El numero ingresado es impar");
-  }
-}
-
-
-
-
-
-
-})
+});
